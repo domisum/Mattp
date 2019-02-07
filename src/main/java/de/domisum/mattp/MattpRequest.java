7242dcaddd3d8da52f0364d138c25bb42fb344d7
domisum <dominik.weissenseel@gmail.com>
2019-02-07 08:36:22 +0100

    Initial commit

---
+++ src/main/java/de/domisum/mattp/MattpRequest.java
@@ -0,0 +1,16 @@
+package de.domisum.mattp;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MattpRequest
+{
+
+	@Getter
+	private final String requestUrl;
+
+
+
+}
